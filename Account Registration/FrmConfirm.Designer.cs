2fc4b795ef3abcc3696e78c47028f8def7514ce3
Villegas.246445 <Villegas.246445@LAB-A-PC34>
2023-09-11 20:28:47 +0800

    Add project files.

---
+++ Account Registration/FrmConfirm.Designer.cs
@@ -0,0 +1,262 @@
+﻿namespace Account_Registration
+{
+    partial class FrmConfirm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            label1 = new Label();
+            label2 = new Label();
+            label3 = new Label();
+            label4 = new Label();
+            label5 = new Label();
+            label6 = new Label();
+            label7 = new Label();
+            label8 = new Label();
+            address = new Label();
+            program = new Label();
+            label11 = new Label();
+            lastname = new Label();
+            firstname = new Label();
+            middlename = new Label();
+            age = new Label();
+            contact = new Label();
+            adds = new Label();
+            button1 = new Button();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(31, 61);
+            label1.Name = "label1";
+            label1.Size = new Size(76, 15);
+            label1.TabIndex = 0;
+            label1.Text = "Student No. :";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(33, 102);
+            label2.Name = "label2";
+            label2.Size = new Size(59, 15);
+            label2.TabIndex = 1;
+            label2.Text = "Program :";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(31, 149);
+            label3.Name = "label3";
+            label3.Size = new Size(67, 15);
+            label3.TabIndex = 2;
+            label3.Text = "Last name :";
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Location = new Point(33, 193);
+            label4.Name = "label4";
+            label4.Size = new Size(68, 15);
+            label4.TabIndex = 3;
+            label4.Text = "First name :";
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Location = new Point(33, 240);
+            label5.Name = "label5";
+            label5.Size = new Size(83, 15);
+            label5.TabIndex = 4;
+            label5.Text = "Middle name :";
+            // 
+            // label6
+            // 
+            label6.AutoSize = true;
+            label6.Location = new Point(33, 288);
+            label6.Name = "label6";
+            label6.Size = new Size(34, 15);
+            label6.TabIndex = 5;
+            label6.Text = "Age :";
+            // 
+            // label7
+            // 
+            label7.AutoSize = true;
+            label7.Location = new Point(30, 322);
+            label7.Name = "label7";
+            label7.Size = new Size(77, 15);
+            label7.TabIndex = 6;
+            label7.Text = "Contact No. :";
+            // 
+            // label8
+            // 
+            label8.AutoSize = true;
+            label8.Location = new Point(31, 367);
+            label8.Name = "label8";
+            label8.Size = new Size(58, 15);
+            label8.TabIndex = 7;
+            label8.Text = "Address : ";
+            // 
+            // address
+            // 
+            address.AutoSize = true;
+            address.Location = new Point(131, 61);
+            address.Name = "address";
+            address.Size = new Size(0, 15);
+            address.TabIndex = 8;
+            // 
+            // program
+            // 
+            program.AutoSize = true;
+            program.Location = new Point(131, 102);
+            program.Name = "program";
+            program.Size = new Size(44, 15);
+            program.TabIndex = 9;
+            program.Text = "label10";
+            // 
+            // label11
+            // 
+            label11.AutoSize = true;
+            label11.Location = new Point(131, 310);
+            label11.Name = "label11";
+            label11.Size = new Size(0, 15);
+            label11.TabIndex = 10;
+            // 
+            // lastname
+            // 
+            lastname.AutoSize = true;
+            lastname.Location = new Point(131, 149);
+            lastname.Name = "lastname";
+            lastname.Size = new Size(44, 15);
+            lastname.TabIndex = 11;
+            lastname.Text = "label12";
+            // 
+            // firstname
+            // 
+            firstname.AutoSize = true;
+            firstname.Location = new Point(131, 193);
+            firstname.Name = "firstname";
+            firstname.Size = new Size(44, 15);
+            firstname.TabIndex = 12;
+            firstname.Text = "label13";
+            // 
+            // middlename
+            // 
+            middlename.AutoSize = true;
+            middlename.Location = new Point(131, 240);
+            middlename.Name = "middlename";
+            middlename.Size = new Size(44, 15);
+            middlename.TabIndex = 13;
+            middlename.Text = "label14";
+            // 
+            // age
+            // 
+            age.AutoSize = true;
+            age.Location = new Point(131, 288);
+            age.Name = "age";
+            age.Size = new Size(44, 15);
+            age.TabIndex = 14;
+            age.Text = "label15";
+            // 
+            // contact
+            // 
+            contact.AutoSize = true;
+            contact.Location = new Point(131, 325);
+            contact.Name = "contact";
+            contact.Size = new Size(44, 15);
+            contact.TabIndex = 15;
+            contact.Text = "label16";
+            // 
+            // adds
+            // 
+            adds.AutoSize = true;
+            adds.Location = new Point(131, 367);
+            adds.Name = "adds";
+            adds.Size = new Size(44, 15);
+            adds.TabIndex = 16;
+            adds.Text = "label17";
+            // 
+            // button1
+            // 
+            button1.Location = new Point(164, 438);
+            button1.Name = "button1";
+            button1.Size = new Size(125, 40);
+            button1.TabIndex = 17;
+            button1.Text = "button1";
+            button1.UseVisualStyleBackColor = true;
+            // 
+            // FrmConfirm
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(479, 503);
+            Controls.Add(button1);
+            Controls.Add(adds);
+            Controls.Add(contact);
+            Controls.Add(age);
+            Controls.Add(middlename);
+            Controls.Add(firstname);
+            Controls.Add(lastname);
+            Controls.Add(label11);
+            Controls.Add(program);
+            Controls.Add(address);
+            Controls.Add(label8);
+            Controls.Add(label7);
+            Controls.Add(label6);
+            Controls.Add(label5);
+            Controls.Add(label4);
+            Controls.Add(label3);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Name = "FrmConfirm";
+            Text = "FrmConfirm";
+            Load += FrmConfirm_Load;
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label label1;
+        private Label label2;
+        private Label label3;
+        private Label label4;
+        private Label label5;
+        private Label label6;
+        private Label label7;
+        private Label label8;
+        private Label address;
+        private Label program;
+        private Label label11;
+        private Label lastname;
+        private Label firstname;
+        private Label middlename;
+        private Label age;
+        private Label contact;
+        private Label adds;
+        private Button button1;
+    }
+}
